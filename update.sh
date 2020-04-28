3c9f0aa6444410753de80dea6b3f0b117cd60ddd
AndrewDaws <AndrewDaws@users.noreply.github.com>
2020-04-28 11:50:36 -0400

    Merged scripts and moved update script to base directory.

--- update.sh
+++ update.sh
@@ -1,6 +1,40 @@
 #!/bin/bash
 script_name="$(basename "${0}")"
 
+# Format columns and remove unwanted characters
+format_file() {
+  # Format each individual line
+  rm -f "${1}-formatted"
+  while read currentLine; do
+    currentASN="$(echo "${currentLine}" | grep -o -P '^[^,]+' | grep -o -P '[0-9]+')"
+
+    # Check if current line contains an ASN
+    if [[ -n "${currentASN}" ]]; then
+      currentEntity="$( \
+        echo "${currentLine}" \
+        | grep -o -P '^\"?[0-9]+\"?,\s*\K.*$' \
+        | sed 's/^"\([^"]\)/\1/' \
+        | sed 's/["/]\+[^"/]*$//')"
+      echo "${currentASN},\"${currentEntity}\"" >> "${1}-formatted"
+    fi
+  done < "${1}"
+  rm -f "${1}"
+  (echo "ASN,Entity"; cat "${1}-formatted") > "${1}"
+  rm -f "${1}-formatted"
+}
+
+# Fix invalid lines, sort, and remove duplicates
+clean_file() {
+  rm -f "${1}-cleaned"
+  grep -o -P '^\"?[0-9]+\"?,\"?.+\"?$' "${1}" \
+    | sort -n -t ',' -k 1,2 \
+    | sort -n -u -t ',' -k 1,1 \
+    > "${1}-cleaned"
+  rm -f "${1}"
+  (echo "ASN,Entity"; cat "${1}-cleaned") > "${1}"
+  rm -f "${1}-cleaned"
+}
+
 # Download GeoIP ASN lists
 download_geoip_lists() {
   # Find current MaxMind license key
@@ -117,6 +151,8 @@ main() {
     exit 1
   fi
 
+  format_file "${input_file}"
+  clean_file "${input_file}"
   update_file "${input_file}"
 }
 
